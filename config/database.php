e57467f3a59752cc50efa797adac85e7cada7033
hugoresende27 <Hugoresende27@gmail.com>
2022-02-13 03:14:45 +0000

    make it better

--- config/database.php
+++ config/database.php
@@ -19,7 +19,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
