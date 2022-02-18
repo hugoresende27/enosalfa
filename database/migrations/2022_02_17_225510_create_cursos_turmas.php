ebbada5645877b1a03a5abcd00b86bed05380438
hugoresende27 <Hugoresende27@gmail.com>
2022-02-18 10:21:05 +0000

    'ma'

--- database/migrations/2022_02_17_225510_create_cursos_turmas.php
+++ database/migrations/2022_02_17_225510_create_cursos_turmas.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('cursos_turmas', function (Blueprint $table) {
             
-
+            $table->id();
             $table->unsignedBigInteger('id_curso')->nullable();
             $table->unsignedBigInteger('id_turma')->nullable();
            
