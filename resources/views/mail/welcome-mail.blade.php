d096e5c852e6c713b3c8c557c27ee6ac7def3b3f
hugoresende27 <Hugoresende27@gmail.com>
2022-02-23 10:58:09 +0000

    'tau'

--- resources/views/mail/welcome-mail.blade.php
+++ resources/views/mail/welcome-mail.blade.php
@@ -51,7 +51,7 @@
         <img class="card-img-top minha-img" src="https://cdn.pixabay.com/photo/2015/04/01/11/08/sillouette-702178_960_720.jpg" alt="Card image cap">
         <div class="card-body">
             <a href="https://enos-alfa.herokuapp.com/" class="btn btn-primary">
-          <h5 class=" text-center">Software Enos</h5>
+          <h5 class=" text-center">Enos</h5>
           <p class="card-text">Conheça o software Enos</p>
           </a>
         </div>
